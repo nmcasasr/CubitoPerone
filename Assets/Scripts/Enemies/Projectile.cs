b82dfe205dd808e4958ed2350f4507c79eea69dd
Juan Camilo Cardenas Gomez <juaccardenasgom@unal.edu.co>
2020-07-25 01:33:18 -0500

    Merged and stable, movement & enemy(range)

---
+++ Assets/Scripts/Enemies/Projectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            //TODO: Take Damage Logic
+            Destroy(gameObject);
+        }
+    }
+}
