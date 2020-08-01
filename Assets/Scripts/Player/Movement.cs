6a7fb4355569dcf829bd6b86e4c9f7a04880a5bc
unknown <mescamillas@unal.edu.co>
2020-08-01 05:22:18 +0000

    plataforma y script inicial

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -17,6 +17,7 @@ public class Movement : MonoBehaviour
     Vector2 initBCOffeset;
     bool bCanJump;
     AnimationsController aController;
+
     // Start is called before the first frame update
     void Start()
     {
