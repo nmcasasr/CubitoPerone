45eb407ed2a5f5d20df45918a53ff00eeb03b618
Nicolas Mateo Casas Ramos <nmcasasr@unal.edu.co>
2020-08-12 22:36:39 -0500

    damage zone script

---
+++ Assets/Scripts/Environment/DamageZone.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageZone : MonoBehaviour
+{
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.gameObject.GetComponent<PlayerHealth>().takeDamage(100000);
+    }
+}
