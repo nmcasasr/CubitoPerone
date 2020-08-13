dab347fb936282e051c2a2ed9e9f256b4582e38a
Nicolas Mateo Casas Ramos <nmcasasr@unal.edu.co>
2020-08-12 20:53:27 -0500

    parallax script

---
+++ Assets/Scripts/Environment/ParallaxComponent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxComponent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Vector2 parallaxMultiplier;
+
+    private Transform cameraTransform;
+    private Vector3 lastCameraPostion;
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraPostion = cameraTransform.position;
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPostion;
+        transform.position += new Vector3(deltaMovement.x * parallaxMultiplier.x, deltaMovement.y * parallaxMultiplier.y);
+        lastCameraPostion = cameraTransform.position;
+    }
+}
