70c41aa821baf05078e29ec201ebc4009a220652
Nicolas Mateo Casas Ramos <nmcasasr@unal.edu.co>
2020-08-15 03:09:36 -0500

    new version

---
+++ Assets/Scripts/Environment/MusicEnd.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicEnd : MonoBehaviour
+{
+    int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        index = SceneManager.GetActiveScene().buildIndex;
+        if(!(index == 0))
+        {
+            DontDestroyOnLoad(gameObject); /// taken from this Tutorial https://youtu.be/x9lguwc0Pyk
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        index = SceneManager.GetActiveScene().buildIndex;
+        if (index == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
