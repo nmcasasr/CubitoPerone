acb32e2fb5130a7e71fbdfe42e1833f01fee6ad5
Nicolas Mateo Casas Ramos <nmcasasr@unal.edu.co>
2020-08-15 03:31:06 -0500

    fix

--- Assets/Scripts/Environment/DamageZone.cs
+++ Assets/Scripts/Environment/DamageZone.cs
@@ -6,6 +6,10 @@ public class DamageZone : MonoBehaviour
 {
     public void OnTriggerStay2D(Collider2D collision)
     {
+        if (collision.CompareTag("Player"))
+        {
         collision.gameObject.GetComponent<PlayerHealth>().takeDamage(100000);
+        }
+        
     }
 }
